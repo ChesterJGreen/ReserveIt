f8b6dc38a52b52a725b9b229f98c0f7745ffc4d7
cj.green <cj.green@intimetec.com>
2021-11-22 11:34:36 -0700

    Add project files.

---
+++ ReserveIt/Enums/VenueTimeZone.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReserveIt.Enums
+{
+    public enum VenueTimeZone
+    {
+        PST,
+        MST,
+        EST,
+        CST
+    }
+}
---
+++ ReserveIt/Models/ConfereneceRoom.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ReserveIt.Enums;
+
+namespace ReserveIt.Models
+{
+    public class ConfereneceRoom
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public VenueTimeZone TimeZone{ get; set; }
+        public List<TimeSlot> TimeSlots { get; set; }
+
+        public class TimeSlot { 
+            public string Name { get; set; }
+            public int Time { get; set; }
+        }
+        
+    }
+}
